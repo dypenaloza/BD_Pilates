aefa05ea1e472df78df4282911dc5e847daca045
Dayana Peñaloza <daypenalozaduarte@gmail.com>
2026-06-22 14:02:53 -0300

    Agrega consultas

--- 03_consultas.sql
+++ 03_consultas.sql
@@ -1,18 +1,28 @@
 --Consultas
 
+--1)
 --Alumnos activos con su plan vigente
 -- listar alumnos activos junto con el plan que tienen
 --contratado, cantidad de clases y fecha de vencimiento.
 
-SELECT a.ID_Alumno, a.Nombre_Alumno + ' ' + a.Apellido_Alumno as Nombre_Completo,
-p.Nombre_Plan, m.Clases_Disponibles, DATEADD(DAY,p.Duracion_Dias, m.Fecha_Inicio) as Fecha_Vencimiento
+SELECT 
+    a.ID_Alumno,
+    a.Nombre_Alumno + ' ' + a.Apellido_Alumno as Nombre_Completo,
+    p.Nombre_Plan,
+    m.Clases_Disponibles,
+    DATEADD(DAY,p.Duracion_Dias, m.Fecha_Inicio) as Fecha_Vencimiento
 FROM Alumnos AS a
-JOIN Matriculas AS m ON a.ID_Alumno = m.ID_Alumno
-JOIN Planes AS p ON p.ID_Plan = m.ID_Plan
-WHERE a.Estado_Alumno = 'Activo' AND m.Estado_Matricula = 'Activa'
-AND ( DATEADD(DAY,p.Duracion_Dias, m.Fecha_Inicio) >= GETDATE() )
+JOIN Matriculas AS m
+    ON a.ID_Alumno = m.ID_Alumno
+JOIN Planes AS p
+    ON p.ID_Plan = m.ID_Plan
+WHERE
+    a.Estado_Alumno = 'Activo'
+    AND m.Estado_Matricula = 'Activa'
+    AND ( DATEADD(DAY,p.Duracion_Dias, m.Fecha_Inicio) >= GETDATE() )
 
 
+--2)
 -- Clases disponibles por sede
 --mostrar clases que todavía tienen cupo disponible,
 --indicando sede, profesor, horario y cupos restantes.
@@ -45,8 +55,121 @@ GROUP BY
 HAVING
     s.Cantidad_Reformers - COUNT(r.ID_Reserva) > 0;
 
---Motivos de cancelación más frecuente
+--3)
+--Clases con mayor demanda
+--obtener las clases con más reservas totales.
 
+SELECT 
+    C.ID_Clase,
+    C.Nombre_Clase AS Clase,
+    C.Fecha_Clase,
+    C.Hora_Inicio,
+    C.Hora_Fin,
+    --Cuenta los ID de Reserva por que el GROUP BY
+    --Ya agrupa según el ID Clase
+    COUNT(R.ID_Reserva) AS Total_Reservas_Activas
+FROM Clases C
+INNER JOIN Reservas R
+    --Inner join ya que solo obtenemos las clases que tuvieron reserva
+    ON C.ID_Clase = R.ID_Clase
+WHERE 
+    --Y las reservas no fueron canceladas
+    R.Fecha_Cancelacion IS NULL
+GROUP BY 
+    --Agrupo por clase, pero cuento reservas.
+    C.ID_Clase,
+    C.Nombre_Clase,
+    C.Fecha_Clase,
+    C.Hora_Inicio,
+    C.Hora_Fin
+ORDER BY Total_Reservas_Activas DESC;
+
+--4)
+-- Clases con menor demanda
+--detectar clases con pocas reservas o baja ocupación.
+
+SELECT 
+    C.ID_Clase,
+    C.Nombre_Clase AS Clase,
+    C.Fecha_Clase,
+    C.Hora_Inicio,
+    C.Hora_Fin,
+    S.Cantidad_Reformers,
+    --Misma lógica que el anterior, cuenta por IDReserva ya que se agrupa
+    --por IDClase
+    COUNT(R.ID_Reserva) AS Total_Reservas,
+    --Cast: Convierte el resultado del la cuenta en Decimal(5,2)
+    --Sirve para tener una columna que exprese la ocupación como porcentaje
+    CAST(COUNT(R.ID_Reserva) * 100.0 / S.Cantidad_Reformers AS DECIMAL(5,2)) AS Porcentaje_Ocupacion
+FROM Clases C
+JOIN Salones S
+    ON C.ID_Salon = S.ID_Salon
+--Se hace LEFT JOIN por que puede haber clases sin reservas, importa para
+--la baja demanda
+LEFT JOIN Reservas R
+    ON C.ID_Clase = R.ID_Clase
+GROUP BY 
+    C.ID_Clase,
+    C.Nombre_Clase,
+    C.Fecha_Clase,
+    C.Hora_Inicio,
+    C.Hora_Fin,
+    S.Cantidad_Reformers
+--Ordeno asc para que primero queden las de más baja ocupación
+ORDER BY Porcentaje_Ocupacion ASC;
+
+
+--5)
+--Asistencia por alumno
+--calcular cuántas clases asistió, cuántas faltó y cuántas canceló cada alumno
+
+SELECT
+    A.ID_Alumno,
+    A.Nombre_Alumno,
+    A.Apellido_Alumno,
+    COUNT(R.ID_Reserva) AS Total_Reservas,
+    --Caso 1: Asistió y no canceló
+    --Cuando asistió se suma uno, si no asistió no se suma nada
+    --Se guarda en Clases_Asistidas
+    --(Cómo se agrupa por ID_Alumno, queda cada reserva asociada al alumno)
+    SUM(CASE
+            WHEN R.Asistio = 1
+            THEN 1
+            ELSE 0
+        END) AS Clases_Asistidas,
+    
+    --Caso 2: No canceló pero faltó
+    SUM(CASE
+            WHEN R.Asistio = 0 
+             AND R.Fecha_Cancelacion IS NULL
+            THEN 1
+            ELSE 0
+        END) AS Clases_Faltadas,
+    --Caso 3: Canceló
+    SUM(CASE
+            WHEN R.Fecha_Cancelacion IS NOT NULL
+            THEN 1
+            ELSE 0
+        END) AS Clases_Canceladas
+
+FROM Alumnos A
+--Left Join por que puede ser que un alumno no haya reservado
+LEFT JOIN Reservas R
+    ON A.ID_Alumno = R.ID_Alumno
+
+GROUP BY
+    A.ID_Alumno,
+    A.Nombre_Alumno,
+    A.Apellido_Alumno
+--Ordenamos por apellido y nombre asi queda alfabético
+ORDER BY
+    A.Apellido_Alumno,
+    A.Nombre_Alumno;
+
+
+
+--8)
+--Motivos de cancelación más frecuente
 SELECT
     m.Desc_Motivo,
     contador.Veces
